a7b9d2e469c430ce09f9a05cb17772a28a31668d
Brett Zamir <brettz9@yahoo.com>
2015-01-12 10:47:35 -0700

    utilize clarinet in testing

--- test/test.basic.js
+++ test/test.basic.js
@@ -3,7 +3,8 @@
 (function () {'use strict';
 
 var dj = require('../lib/DJ'),
-    testCase = require('nodeunit').testCase;
+    testCase = require('nodeunit').testCase,
+    clarinetAdapter = require('../lib/DJ.clarinet');
 
 var json = {
     "store": {
@@ -27,6 +28,53 @@ module.exports = testCase({
         test.expect(1);
         var expected;
         var result = dj();
+        test.deepEqual(expected, result);
+        test.done();
+    },
+    'clarinet': function (test) {
+        test.expect(1);
+
+        var clarinet = require('clarinet');
+        var parser = clarinet.parser();
+
+        function addMethods (parser) {
+            parser.onerror = function (e) {
+                // an error happened. e is the error.
+            };
+            parser.onvalue = function (v) {
+                // got some value.  v is the value. can be string, double, bool, or null.
+            };
+            parser.onopenobject = function (key) {
+                // opened an object. key is the first key.
+            };
+            parser.onkey = function (key) {
+                // got a key in an object.
+            };
+            parser.oncloseobject = function () {
+                // closed an object.
+            };
+            parser.onopenarray = function () {
+                // opened an array.
+            };
+            parser.onclosearray = function () {
+                // closed an array.
+            };
+            parser.onend = function () {
+                // parser stream is done, and ready to have more stuff written to it.
+            };
+        }
+        addMethods(parser);
+        
+        parser.write('{"foo": "bar"}').close();
+        
+        var expected;
+        var djParser = clarinetAdapter.DJClarinet;
+        
+        addMethods(djParser);
+        
+        djParser.write('{"foo": "bar"}').close();
+        
+        var result;
         test.deepEqual(expected, result);
         test.done();
     }
