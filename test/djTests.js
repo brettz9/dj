0c7d09eb1092c3697e335bbe943a18fea26a3ac2
Brett Zamir <brettz9@yahoo.com>
2016-01-11 16:03:52 -0700

    Reset string value after each transform; fix tests

--- test/djTests.js
+++ test/djTests.js
@@ -2,30 +2,25 @@ var DJ, Stringifier, exports;
 
 var djTests = {
     'basic test': function (test) {
-        test.expect(1);
+        test.expect(7);
 
         var sf = DJ.Stringifier();
         var sfJS = DJ.Stringifier({mode: 'JavaScript'});
 
-        console.log(sf.walkJSONObject(null));
-        console.log(sf.walkJSONObject(true));
-        console.log(sf.walkJSONObject(4e5));
-        console.log(sf.walkJSONObject('abc'));
-        console.log(sf.walkJSONObject(['childItem', 'childWith"Dbl"Quotes']));
+        test.strictEqual('null', sf.walkJSONObject(null));
+        test.strictEqual('true', sf.walkJSONObject(true));
+        test.strictEqual('400000', sf.walkJSONObject(4e5));
+        test.strictEqual('"abc"', sf.walkJSONObject('abc'));
+        test.strictEqual('["childItem","childWith\\"Dbl\\"Quotes"]', sf.walkJSONObject(['childItem', 'childWith"Dbl"Quotes']));
 
-        console.log(
+        test.strictEqual('{"a":null,"b":true,"c":3,"d":["childItem","childWith\\"Dbl\\"Quotes"],"e":{"nested":true},"f":null}',
             sf.walkJSONObject({a: null, b: true, c: 3, d: ['childItem', 'childWith"Dbl"Quotes'], e: {nested: true}, f: Infinity})
         );
 
-        console.log(
+        test.strictEqual('{"a":null,"b":true,"c":3,"d":["childItem","childWith\\"Dbl\\"Quotes"],"e":{"nested":true},"f":Infinity,"g":undefined,"h":function () {}}',
             sfJS.walkJSONObject({a: null, b: true, c: 3, d: ['childItem', 'childWith"Dbl"Quotes'], e: {nested: true}, f: Infinity, g: undefined, h: function () {}})
         );
 
-        var expected = '';
-
-        var result;
-
-        test.deepEqual(expected, result);
         test.done();
     }
 };
