a71294ce78e35ae8adef1c540b5ae1e9f26e2295
Brett Zamir <brettz9@yahoo.com>
2016-01-11 15:26:25 -0700

    Begin adding nodeunit tests for browser

---
+++ test/djTests.js
@@ -0,0 +1,41 @@
+var DJ, Stringifier, exports;
+
+if (exports) {
+    Stringifier = require('./DJ.Stringifier');
+}
+
+var djTests = {
+    'basic test': function (test) {
+        test.expect(1);
+
+
+        var sf = Stringifier();
+        var sfJS = Stringifier({mode: 'JavaScript'});
+
+        console.log(sf.walkJSONObject(null));
+        console.log(sf.walkJSONObject(true));
+        console.log(sf.walkJSONObject(4e5));
+        console.log(sf.walkJSONObject('abc'));
+        console.log(sf.walkJSONObject(['childItem', 'childWith"Dbl"Quotes']));
+
+        console.log(
+            sf.walkJSONObject({a: null, b: true, c: 3, d: ['childItem', 'childWith"Dbl"Quotes'], e: {nested: true}, f: Infinity})
+        );
+
+        console.log(
+            sfJS.walkJSONObject({a: null, b: true, c: 3, d: ['childItem', 'childWith"Dbl"Quotes'], e: {nested: true}, f: Infinity, g: undefined, h: function () {}})
+        );
+
+        var expected = '';
+
+        var result;
+
+        test.deepEqual(expected, result);
+        test.done();
+    }
+};
+
+if (typeof exports !== 'undefined') {
+    DJ = require('../lib/DJ');
+    module.exports = djTests;
+}
